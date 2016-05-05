0459096f154ff6279397397fa4a3efb631927fd1
Full <fullbl@mail.com>
2016-05-05 19:38:54 +0200

    basic room form in admin

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -6,10 +6,33 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Room;
+
 /**
- * This class is responsible for updating room resources
+ * This class is responsible for room resources CRUD
  */
 class RoomController extends \App\Http\Controllers\Controller
 {
-    //
+	/** 
+	 * show a listing
+	 * @return array Room
+	 */
+    public function index(){
+    	return Room::all();
+    }
+
+
+	/**
+	 * save a new room
+	 * @return Room object just created
+	 */
+    public function store( Request $request ){
+    	$this->validate($request, [
+		    'title' => 'required|unique:posts|max:255',
+		    'author.name' => 'required',
+		    'author.description' => 'required',
+		]);
+
+    	return Room::all();
+    }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,23 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Loading dashboard
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">New room</div>
+                <div class="panel-body">
+                    <form method="POST" method="{{route('api.room.create')" role="form">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="beds">Beds:</label>
+                            <input type="number" class="form-control" id="beds">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
